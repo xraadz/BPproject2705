b0160ea4be3b3ce77b49dbd01e14cf688d010ecf
xraadz <rioraditya37@gmail.com>
2023-10-08 15:54:33 +0700

    pembaruan

--- app/src/main/java/com/example/projectbp2705/LoginActivity.kt
+++ app/src/main/java/com/example/projectbp2705/LoginActivity.kt
@@ -12,21 +12,27 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btnLogin:Button = findViewById(R.id.btnLogin)
-        val txtUsername:EditText = findViewById(R.id.editTextUsername)
-        val txtPassword:EditText = findViewById(R.id.editTextPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val txtUsername: EditText = findViewById(R.id.editTextUsername)
+        val txtPassword: EditText = findViewById(R.id.editTextPassword)
         val logObj = Login()
 
         logObj.username = txtUsername.text.toString()
         logObj.password = txtPassword.text.toString()
 
         btnLogin.setOnClickListener {
-        if(logObj.loginCheck() == true){
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
-        }else{
-            Toast.makeText(this,"Login gagal, cek username dan password",
-                Toast.LENGTH_SHORT).show()
+            if (txtUsername.text.toString().equals("raditz") &&
+                txtPassword.text.toString().equals("amikom")) {
+                if (logObj.loginCheck() == true) {
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(
+                        this, "Login gagal, cek username dan password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 }
